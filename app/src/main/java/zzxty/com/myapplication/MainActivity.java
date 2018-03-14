0a2849018e3b3169ada2036ff0d60be2b077f1b9
HNKJXY <344376588@qq.com>
2018-03-14 15:09:24 +0800

    添加了一行代码

--- app/src/main/java/zzxty/com/myapplication/MainActivity.java
+++ app/src/main/java/zzxty/com/myapplication/MainActivity.java
@@ -7,6 +7,8 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        //第一个目标
     }
 }
